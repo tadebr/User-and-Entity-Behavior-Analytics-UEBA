12e1e655d38bf01a1047338f6e50e24ff4c6236d
tade <tadeleberhanu41@gmail.com>
2025-03-19 16:43:16 +0300

    Update 3-Action_Sequence_Training.py

--- 3-Action_Sequence_Training.py
+++ 3-Action_Sequence_Training.py
@@ -11,10 +11,10 @@ from keras import losses,metrics
 from keras.callbacks import TensorBoard, ModelCheckpoint
 import matplotlib.pyplot as plt
 import warnings 
-from keras.layers import Dense, Activation, Embedding, Conv2D, MaxPooling2D, Reshape, 
-                         BatchNormalization, Dropout, Input, concatenate, 
-                         GlobalAveragePooling2D, Flatten, ConvLSTM2D, LSTM, Conv3D
+from keras.layers import Dense, Activation, Embedding, Conv2D, MaxPooling2D, Reshape,BatchNormalization, Dropout, Input, concatenate,GlobalAveragePooling2D, Flatten, ConvLSTM2D, LSTM, Conv3D
 
+
+                                                                                          ^
 
 
 
